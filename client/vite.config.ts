bfeba6471adae07a6a8d18b400df79b91ba43de0
Kazi Foyez Ahmed <foyezar@gmail.com>
2025-07-06 22:50:49 +0200

    add tailwindcss and import alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
