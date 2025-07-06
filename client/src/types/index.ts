e7d7220ca2fd9e7162ac74ccbe406c801bc5c748
Kazi Foyez Ahmed <foyezar@gmail.com>
2025-07-06 22:51:24 +0200

    add types

---
+++ client/src/types/index.ts
@@ -0,0 +1,30 @@
+export type Status = "queued" | "running" | "done" | "error";
+
+export type URLData = {
+  id: number;
+  title: string;
+  html_version: string;
+  has_login_form: boolean;
+  internal_links: number;
+  external_links: number;
+  status: Status;
+  created_at: string;
+};
+
+export type URLDetails = {
+  id: number;
+  title: string;
+  html_version: string;
+  has_login_form: boolean;
+  internal_links: number;
+  external_links: number;
+  heading_counts: {
+    h1: number;
+    h2: number;
+    h3: number;
+    h4: number;
+    h5: number;
+    h6: number;
+  };
+  broken_links: { link: string; status: number }[];
+};
